12b256a463244c7086abeddeadebdd1ce6c53727
César Liesens <cliesens@gmail.com>
2021-08-28 08:14:16 +0200

    Fix bug introduced by changing Intersection from a struct into a class

--- SharpGo/Game/Intersection.cs
+++ SharpGo/Game/Intersection.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpGo.Game
+﻿using System;
+
+namespace SharpGo.Game
 {
 	public class Intersection
 	{
@@ -7,5 +9,23 @@
 		public int J { get; }
 
 		public Intersection(State state, int i, int j) => (State, I, J) = (state, i, j);
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Intersection o))
+			{
+				return false;
+			}
+
+			return State == o.State && I == o.I && J == o.J;
+		}
+
+		public override int GetHashCode() => Tuple.Create(State, I, J).GetHashCode();
+
+		public static bool operator ==(Intersection x, Intersection y) => x.Equals(y);
+
+		public static bool operator !=(Intersection x, Intersection y) => !x.Equals(y);
+
+		public override string ToString() => $"({State}, {I}, {J})";
 	}
 }
